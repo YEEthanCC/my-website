f4f440a84764e09d8ff8d1a6416d8b147fc2f99b
root <root@LAPTOP-8O10EN7G>
2025-06-01 11:07:00 +0800

    fixed navbar issues

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
     <div className="w-full flex">
-        <div className='w-3/5 m-4'>Ethan's Blogs</div>
+        <div className='w-3/5 m-4'>Ethan&apos;s Blogs</div>
         <Link href="/about" className="m-4 text-blue-500">About me</Link>
         <Link href="/projects" className="m-4 text-blue-500">My works</Link>
         <Link href="/contact" className="m-4 text-blue-500">Contacts</Link>
